ed908e28a93ab50abba927bd7c8a2e8b1b5b95e4
feling-rc <retoz2023@gmail.com>
2026-03-07 23:28:48 -0500

    Update main.py

--- main.py
+++ main.py
@@ -2,11 +2,13 @@ from server import app
 import os
 import threading
 from datoscliente import main as datoscliente_main
-from tareas import main as tareas_main
+from tareas import tareas_bp
+
+# registrar modulo de tareas
+app.register_blueprint(tareas_bp)
 
 if __name__ == "__main__":
     threading.Thread(target=datoscliente_main, daemon=True).start()
-    threading.Thread(target=tareas_main, daemon=True).start()
 
     port = int(os.environ.get("PORT", 5000))
     app.run(host="0.0.0.0", port=port)
