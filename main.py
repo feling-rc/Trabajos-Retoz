745af4c2ebd3f97baea69c97eb0373e25f7fb8ae
feling-rc <retoz2023@gmail.com>
2026-03-07 22:44:00 -0500

    Update main.py

--- main.py
+++ main.py
@@ -2,9 +2,11 @@ from server import app
 import os
 import threading
 from datoscliente import main as datoscliente_main
-
-threading.Thread(target=datoscliente_main, daemon=True).start()
+from tareas import main as tareas_main
 
 if __name__ == "__main__":
+    threading.Thread(target=datoscliente_main, daemon=True).start()
+    threading.Thread(target=tareas_main, daemon=True).start()
+
     port = int(os.environ.get("PORT", 5000))
     app.run(host="0.0.0.0", port=port)
